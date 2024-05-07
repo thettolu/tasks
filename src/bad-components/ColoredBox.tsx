a627cd777c76858e8060b34522a3b6ee00823a86
DracoMeteorStorm <adracometeorstorm@gmail.com>
2024-05-07 12:43:36 -0400

    -

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -4,40 +4,74 @@ import { Button } from "react-bootstrap";
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
-function ChangeColor(): JSX.Element {
-    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
-    return (
-        <Button onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}>
-            Next Color
-        </Button>
-    );
-}
-
-function ColorPreview(): JSX.Element {
-    return (
-        <div
-            data-testid="colored-box"
-            style={{
-                width: "50px",
-                height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
-                display: "inline-block",
-                verticalAlign: "bottom",
-                marginLeft: "5px"
-            }}
-        ></div>
-    );
-}
-
 export function ColoredBox(): JSX.Element {
+    const colorLoop: Record<string, string> = {
+        red: "blue",
+        blue: "green",
+        green: "red"
+    };
+
+    const [currentColor, setCurrentColor] = useState<string>(
+        COLORS[DEFAULT_COLOR_INDEX]
+    );
+    // const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
+
+    function cycleColor(currentIndex: string): void {
+        setCurrentColor(colorLoop[currentIndex]);
+    }
+
     return (
         <div>
-            <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
+            <Button onClick={() => cycleColor(currentColor)}>Next Color</Button>
+            <div
+                data-testid="colored-box"
+                style={{
+                    width: "50px",
+                    height: "50px",
+                    backgroundColor: currentColor,
+                    display: "inline-block",
+                    verticalAlign: "bottom",
+                    marginLeft: "5px"
+                }}
+            ></div>
+
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <h3>Colored Box</h3>
+                <span>The current color is: {currentColor}</span>
+                {/* <div>
+                    <ChangeColor></ChangeColor>
+                    <ColorPreview></ColorPreview>
+                </div> */}
             </div>
         </div>
     );
 }
+
+// function ColorPreview(): JSX.Element {
+//     return (
+//         <div
+//             data-testid="colored-box"
+//             style={{
+//                 width: "50px",
+//                 height: "50px",
+//                 backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+//                 display: "inline-block",
+//                 verticalAlign: "bottom",
+//                 marginLeft: "5px"
+//             }}
+//         ></div>
+//     );
+// }
+
+// export function ColoredBox(): JSX.Element {
+//     return (
+//         <div>
+//             <h3>Colored Box</h3>
+//             <span>The current color is: {currentColor}</span>
+//             <div>
+//                 <ChangeColor></ChangeColor>
+//                 <ColorPreview></ColorPreview>
+//             </div>
+//         </div>
+//     );
+// }
